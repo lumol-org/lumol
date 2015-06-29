e6791fa2181a26e987e224e33a214cd0e492ee35
Luthaf <luthaf@luthaf.fr>
2015-06-29 16:24:44 +0200

    Update the MD example

--- examples/molecular_dynamics.rs
+++ examples/molecular_dynamics.rs
@@ -2,22 +2,27 @@ extern crate cymbalum;
 use cymbalum::*;
 
 fn main() {
-    let mut universe = Universe::from_cell(UnitCell::cubic(20.0));
+    let mut universe = Universe::from_cell(UnitCell::cubic(17.0));
 
-    for i in 0..10 {
-        for j in 0..10 {
-            for k in 0..10 {
+    // Create a cubic crystal of Argon by hand.
+    for i in 0..5 {
+        for j in 0..5 {
+            for k in 0..5 {
                 let mut part = Particle::new("Ar");
                 part.set_position(Vector3D::new(
-                        i as f64 * 2.0,
-                        j as f64 * 2.0,
-                        k as f64 * 2.0
+                        i as f64 * 3.4,
+                        j as f64 * 3.4,
+                        k as f64 * 3.4
                 ));
                 universe.add_particle(part);
             }
         }
     }
     universe.add_pair_interaction("Ar", "Ar", LennardJones{sigma: 3.4, epsilon: 1e-4});
+
+    let mut velocities = BoltzmanVelocities::new(300.0);
+    velocities.seed(129);
+    velocities.init(&mut universe);
 
     let mut simulation = Simulation::new(MolecularDynamics::new(1.0));
     simulation.add_output(TrajectoryOutput::new("trajectory.xyz"));
