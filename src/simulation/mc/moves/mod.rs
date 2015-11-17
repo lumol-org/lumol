9b91813d0d4e364179ab071d36dae8560bbd7fda
Guillaume Fraux <guillaume.fraux@ens.fr>
2015-11-17 21:22:31 +0100

    Reorganize the moves module.

---
+++ src/simulation/mc/moves/mod.rs
@@ -0,0 +1,47 @@
+/* Cymbalum, Molecular Simulation in Rust - Copyright (C) 2015 Guillaume Fraux
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+//! A Monte-Carlo simulation is based on a set of applicables moves.
+//!
+//! For example, NVT Monte-Carlo will use the `Translate` move only for particles
+//! and add the `Rotate` moves for molecules. NPT Monte-Carlo will add the
+//! `VolumeResize` move.
+//!
+//! In all this module, beta refers to the Boltzmann factor 1/(kB T)
+extern crate rand;
+use self::rand::Rng;
+
+use universe::Universe;
+
+/// The `MCMove` trait correspond to the set of methods used in Monte-Carlo
+/// simulations.
+pub trait MCMove {
+    /// Give a short description of this move
+    fn describe(&self) -> &str;
+
+    /// Prepare the move, by selecting the particles to move, and the parameters
+    /// of the move. The `rng` random number generator should be used to
+    /// generate the parameters of the move.
+    fn prepare(&mut self, universe: &mut Universe, rng: &mut Box<Rng>);
+
+    /// Get the cost of performing this move, as the exponential factor. For
+    /// simple NVT simulations, this cost is the energetic difference over
+    /// `beta`.
+    ///
+    /// The cost must be dimmensionless, and will be placed in an exponential.
+    fn cost(&self, universe: &Universe, beta: f64) -> f64;
+
+    /// Effectivelly apply the move, if it has not already been done in
+    /// `prepare`.
+    fn apply(&mut self, universe: &mut Universe);
+
+    /// Restore the universe to it's initial state, if it has been changed in
+    /// `prepare`.
+    fn restore(&mut self, universe: &mut Universe);
+}
+
+mod translate;
+pub use self::translate::Translate;
--- src/simulation/mc/moves/translate.rs
+++ src/simulation/mc/moves/translate.rs
@@ -4,51 +4,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/
  */
-//! A Monte-Carlo simulation is based on a set of applicables moves.
-//!
-//! For example, NVT Monte-Carlo will use the `Translate` move only for particles
-//! and add the `Rotate` moves for molecules. NPT Monte-Carlo will add the
-//! `VolumeResize` move.
-//!
-//! In all this module, beta refers to the Boltzmann factor 1/(kB T)
 extern crate rand;
 use self::rand::distributions::{Sample, Range};
 use self::rand::Rng;
 
 use std::usize;
 
+use super::MCMove;
+
 use types::Vector3D;
 use universe::Universe;
-
-/// The `MCMove` trait correspond to the set of methods used in Monte-Carlo
-/// simulations.
-pub trait MCMove {
-    /// Give a short description of this move
-    fn describe(&self) -> &str;
-
-    /// Prepare the move, by selecting the particles to move, and the parameters
-    /// of the move. The `rng` random number generator should be used to
-    /// generate the parameters of the move.
-    fn prepare(&mut self, universe: &mut Universe, rng: &mut Box<Rng>);
-
-    /// Get the cost of performing this move, as the exponential factor. For
-    /// simple NVT simulations, this cost is the energetic difference over
-    /// `beta`.
-    ///
-    /// The cost must be dimmensionless, and will be placed in an exponential.
-    fn cost(&self, universe: &Universe, beta: f64) -> f64;
-
-    /// Effectivelly apply the move, if it has not already been done in
-    /// `prepare`.
-    fn apply(&mut self, universe: &mut Universe);
-
-    /// Restore the universe to it's initial state, if it has been changed in
-    /// `prepare`.
-    fn restore(&mut self, universe: &mut Universe);
-}
-
-
-/******************************************************************************/
 
 /// Monte-Carlo move for translating a molecule
 pub struct Translate {
@@ -56,7 +21,7 @@ pub struct Translate {
     molecule: usize,
     /// Translation vector
     delta: Vector3D,
-    /// Translation range
+    /// Translation range for random number generation
     delta_range: Range<f64>,
     /// Potential energy before the move
     e_before: f64,
@@ -96,7 +61,7 @@ impl MCMove for Translate {
         self.molecule = rng.gen_range(0, nmols);
 
         self.e_before = universe.potential_energy();
-        for i in universe.molecules()[self.molecule].clone() {
+        for i in &universe.molecules()[self.molecule] {
             universe[i].position = universe[i].position + self.delta;
         }
     }
@@ -111,7 +76,7 @@ impl MCMove for Translate {
     }
 
     fn restore(&mut self, universe: &mut Universe) {
-        for i in universe.molecules()[self.molecule].clone() {
+        for i in &universe.molecules()[self.molecule] {
             universe[i].position = universe[i].position - self.delta;
         }
     }
