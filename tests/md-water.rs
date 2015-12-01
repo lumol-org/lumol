b4b5651b43de3e2cd1910c30902fb54ab6a6fb1b
Guillaume Fraux <guillaume.fraux@ens.fr>
2015-12-01 20:18:55 +0100

    Use a model of water with electrostatics in tests

--- tests/md-water.rs
+++ tests/md-water.rs
@@ -6,20 +6,26 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/
 */
-#![allow(non_snake_case)]
-//! Testing physical properties of 4 molecules of water in a box
+//! Testing physical properties of f-SPC water
 extern crate cymbalum;
 use self::cymbalum::*;
 
 use std::path::Path;
+use std::sync::{Once, ONCE_INIT};
+static START: Once = ONCE_INIT;
 
-fn setup() -> (Simulation, Universe) {
-    let mut universe = Universe::from_cell(UnitCell::cubic(7.0));
+fn setup(potential: &str, n: usize) -> (Simulation, Universe) {
+    let mut universe = Universe::from_cell(UnitCell::cubic(28.0));
 
-    let origins = vec![Vector3D::new(1.0, 1.0, 1.0),
-                       Vector3D::new(5.0, 5.0, 1.0),
-                       Vector3D::new(1.0, 5.0, 5.0),
-                       Vector3D::new(5.0, 1.0, 5.0)];
+    let mut origins = Vec::new();
+    let delta = 28.0 / n as f64;
+    for i in 0..n {
+        for j in 0..n {
+            for k in 0..n {
+                origins.push(Vector3D::new(i as f64 * delta, j as f64 * delta, k as f64 * delta));
+            }
+        }
+    }
 
     let h_1 = Vector3D::new(0.634859709040957, 0.8983057106778469, 0.0);
     let h_2 = Vector3D::new(-0.634859709040957, 0.8983057106778469, 0.0);
@@ -38,7 +44,7 @@ fn setup() -> (Simulation, Universe) {
     }
 
     let data_dir = Path::new(file!()).parent().unwrap();
-    let potentials = data_dir.join("data").join("water.yml");
+    let potentials = data_dir.join("data").join(potential);
     input::read_interactions(&mut universe, potentials).unwrap();
 
     let mut velocities = BoltzmanVelocities::new(units::from(300.0, "K").unwrap());
@@ -51,12 +57,23 @@ fn setup() -> (Simulation, Universe) {
 }
 
 #[test]
-fn constant_energy() {
-    Logger::stdout();
-    let (mut simulation, mut universe) = setup();
+fn constant_energy_ewald() {
+    START.call_once(|| {Logger::stdout();});
+    let (mut simulation, mut universe) = setup("water-ewald.yml", 2);
 
-    let E_initial = universe.total_energy();
+    let e_initial = universe.total_energy();
     simulation.run(&mut universe, 1000);
-    let E_final = universe.total_energy();
-    assert!(f64::abs((E_initial - E_final)/E_final) < 1.1e-2);
+    let e_final = universe.total_energy();
+    assert!(f64::abs((e_initial - e_final)/e_final) < 3e-2);
+}
+
+#[test]
+fn constant_energy_wolf() {
+    START.call_once(|| {Logger::stdout();});
+    let (mut simulation, mut universe) = setup("water-wolf.yml", 5);
+
+    let e_initial = universe.total_energy();
+    simulation.run(&mut universe, 1000);
+    let e_final = universe.total_energy();
+    assert!(f64::abs((e_initial - e_final)/e_final) < 3e-2);
 }
