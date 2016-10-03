d98afaf12888cbede1501816cd1864f8832374ce
Luthaf <luthaf@luthaf.fr>
2016-10-03 23:59:22 +0200

    [doc] Add examples to all Array{2,3} functions

--- src/core/src/types/arrays.rs
+++ src/core/src/types/arrays.rs
@@ -8,22 +8,72 @@ use std::ops::{Index, IndexMut};
 use std::cmp::PartialEq;
 use types::Zero;
 
-/// Two dimmensional tensors, based on ndarray.
+/// Two dimensional tensors, based on ndarray.
 ///
 /// Most of the methods are simply forwarded to ndarray, so also look the doc
-/// for this crate.
+/// for this crate. This array type mainly supports indexing using tuples as
+/// indices and is though as storage backend for multi-dimensional data.
+///
+/// ```
+/// # use lumol::Array2;
+/// let mut a = Array2::zeros((3, 5));
+///
+/// assert_eq!(a[(0, 4)], 0.0);
+///
+/// a[(0, 4)] = 7.0;
+/// assert_eq!(a[(0, 4)], 7.0);
+/// ```
 #[derive(Debug, Clone)]
 pub struct Array2<T>(Array<T, (Ix, Ix)>);
+
+impl<T> Array2<T> {
+    /// Get the shape of the array
+    /// # Examples
+    /// ```
+    /// # use lumol::Array2;
+    /// let a: Array2<f64> = Array2::zeros((3, 5));
+    /// assert_eq!(a.shape(), (3, 5));
+    /// ```
+    pub fn shape(&self) -> (Ix, Ix) {
+        let shape = self.0.shape();
+        (shape[0], shape[1])
+    }
+}
 
 impl<T: Zero + Clone> Array2<T> {
     /// Create a new `Array2` of the specified `size` filled with the
     /// `Zero::zero` return value.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use lumol::Array2;
+    /// let a: Array2<f64> = Array2::zeros((8, 5));
+    /// assert_eq!(a[(6, 2)], 0.0);
+    /// ```
     pub fn zeros(size: (Ix, Ix)) -> Array2<T> {
         Array2(Array::<T, (Ix, Ix)>::zeros(size))
     }
 
     /// Resize the array if the current size is not `size`, and fill the
     /// new array with zeros.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use lumol::Array2;
+    /// let mut a = Array2::zeros((8, 5));
+    ///
+    /// a[(3, 3)] = 42.0;
+    ///
+    /// // This does nothing
+    /// a.resize_if_different((8, 5));
+    /// assert_eq!(a[(3, 3)], 42.0);
+    ///
+    /// // This allocates a new array
+    /// a.resize_if_different((8, 9));
+    /// assert_eq!(a[(3, 3)], 0.0);
+    /// ```
     pub fn resize_if_different(&mut self, size: (Ix, Ix)) {
         if self.0.shape() != &[size.0, size.1] {
             *self = Array2::zeros(size);
@@ -31,16 +81,16 @@ impl<T: Zero + Clone> Array2<T> {
     }
 }
 
-impl<T> Array2<T> {
-    /// Get the shape of the array
-    pub fn shape(&self) -> (Ix, Ix) {
-        let shape = self.0.shape();
-        (shape[0], shape[1])
-    }
-}
-
 impl<T: Clone> Array2<T> {
-    /// Assign the given scalar to all entry in this array
+    /// Assign the given scalar to all entries in this array
+    /// # Examples
+    /// ```
+    /// # use lumol::Array2;
+    /// let mut a = Array2::zeros((8, 5));
+    /// a.assign(33.0);
+    ///
+    /// assert_eq!(a[(3, 4)], 33.0);
+    /// ```
     pub fn assign(&mut self, value: T) {
         self.0.assign_scalar(&value);
     }
@@ -49,7 +99,17 @@ impl<T: Clone> Array2<T> {
 impl<T: Default> Array2<T> {
     /// Create a new `Array2` of the specified `size` filled with the
     /// `Default::default` return value.
-    pub fn default(size: (Ix, Ix)) -> Array2<T>{
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use lumol::Array2;
+    /// let a: Array2<f64> = Array2::zeros((8, 5));
+    /// let b: Array2<f64> = Array2::default((8, 5));
+    ///
+    /// assert_eq!(a, b);
+    /// ```
+    pub fn default(size: (Ix, Ix)) -> Array2<T> {
         Array2(Array::<T, (Ix, Ix)>::default(size))
     }
 }
@@ -81,22 +141,72 @@ impl<T> IndexMut<(Ix, Ix)> for Array2<T> {
 
 /******************************************************************************/
 
-/// Three dimmensional tensors, based on ndarray
+/// Three dimensional tensors, based on ndarray
 ///
 /// Most of the methods are simply forwarded to ndarray, so also look the doc
-/// for this crate.
+/// for this crate. This array type mainly supports indexing using tuples as
+/// indices and is though as storage backend for multi-dimensional data.
+///
+/// ```
+/// # use lumol::Array3;
+/// let mut a = Array3::zeros((3, 5, 2));
+///
+/// assert_eq!(a[(0, 4, 1)], 0.0);
+///
+/// a[(0, 4, 1)] = 7.0;
+/// assert_eq!(a[(0, 4, 1)], 7.0);
+/// ```
 #[derive(Debug, Clone)]
 pub struct Array3<T>(Array<T, (Ix, Ix, Ix)>);
+
+impl<T> Array3<T> {
+    /// Get the shape of the array.
+    /// # Examples
+    /// ```
+    /// # use lumol::Array3;
+    /// let a: Array3<f64> = Array3::zeros((3, 5, 7));
+    /// assert_eq!(a.shape(), (3, 5, 7));
+    /// ```
+    pub fn shape(&self) -> (Ix, Ix, Ix) {
+        let shape = self.0.shape();
+        (shape[0], shape[1], shape[2])
+    }
+}
 
 impl<T: Zero + Clone> Array3<T> {
     /// Create a new `Array3` of the specified `size` filled with the
     /// `Zero::zero` return value.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use lumol::Array3;
+    /// let a: Array3<f64> = Array3::zeros((8, 5, 2));
+    /// assert_eq!(a[(6, 2, 0)], 0.0);
+    /// ```
     pub fn zeros(size: (Ix, Ix, Ix)) -> Array3<T> {
         Array3(Array::<T, (Ix, Ix, Ix)>::zeros(size))
     }
 
     /// Resize the array if the current size is not `size`, and fill the
     /// new array with zeros.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use lumol::Array3;
+    /// let mut a = Array3::zeros((8, 5, 7));
+    ///
+    /// a[(3, 3, 3)] = 42.0;
+    ///
+    /// // This does nothing
+    /// a.resize_if_different((8, 5, 7));
+    /// assert_eq!(a[(3, 3, 3)], 42.0);
+    ///
+    /// // This allocates a new array
+    /// a.resize_if_different((8, 5, 6));
+    /// assert_eq!(a[(3, 3, 3)], 0.0);
+    /// ```
     pub fn resize_if_different(&mut self, size: (Ix, Ix, Ix)) {
         if self.0.shape() != &[size.0, size.1, size.2] {
             *self = Array3::zeros(size);
@@ -104,16 +214,8 @@ impl<T: Zero + Clone> Array3<T> {
     }
 }
 
-impl<T> Array3<T> {
-    /// Get the shape of the array
-    pub fn shape(&self) -> (Ix, Ix, Ix) {
-        let shape = self.0.shape();
-        (shape[0], shape[1], shape[2])
-    }
-}
-
 impl<T: Clone> Array3<T> {
-    /// Assign the given scalar to all entry in this array
+    /// Assign the given scalar to all entries in this array
     pub fn assign(&mut self, value: T) {
         self.0.assign_scalar(&value);
     }
@@ -122,6 +224,17 @@ impl<T: Clone> Array3<T> {
 impl<T: Default> Array3<T> {
     /// Create a new `Array3` of the specified `size` filled with the
     /// `Default::default` return value.
+    /// `Default::default` return value.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use lumol::Array3;
+    /// let a: Array3<f64> = Array3::zeros((8, 5, 2));
+    /// let b: Array3<f64> = Array3::default((8, 5, 2));
+    ///
+    /// assert_eq!(a, b);
+    /// ```
     pub fn default(size: (Ix, Ix, Ix)) -> Array3<T>{
         Array3(Array::<T, (Ix, Ix, Ix)>::default(size))
     }
