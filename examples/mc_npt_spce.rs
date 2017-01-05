6234cb64407a22e01495e5ca5c89ad7ea74d8da7
Gernot Bauer <bauer@itt.uni-stuttgart.de>
2017-01-06 00:42:33 +0100

    Added example for SPCE water.

--- examples/mc_npt_spce.rs
+++ examples/mc_npt_spce.rs
@@ -7,7 +7,7 @@ use lumol::Logger;
 use lumol::sys::{UnitCell, System, Trajectory};
 use lumol::units;
 use lumol::sim::Simulation;
-use lumol::energy::{LennardJones, PairInteraction, PairRestriction, NullPotential};
+use lumol::energy::{LennardJones, PairInteraction, PairRestriction, NullPotential, Ewald};
 use lumol::sim::mc::{MonteCarlo, Translate, Rotate, Resize};
 use lumol::out::{EnergyOutput, PropertiesOutput, TrajectoryOutput}; 
 
@@ -17,33 +17,49 @@ static START: Once = ONCE_INIT;
 
 fn get_system() -> System {
     let data_dir = Path::new(file!()).parent().unwrap().join("data");
-    let configuration = data_dir.join("ethane.xyz");
+    let configuration = data_dir.join("spce.xyz");
     let mut system = Trajectory::open(configuration)
                                 .and_then(|mut traj| traj.read_guess_bonds())
                                 .unwrap();
-    system.set_cell(UnitCell::cubic(100.0));
+    system.set_cell(UnitCell::cubic(20.0));
 
     // Add intermolecular interactions
-    // TraPPE parameters for CH3 - note that masses are not correct.
     let lj = Box::new(LennardJones{
-        sigma: units::from(3.750, "A").unwrap(),
-        epsilon: units::from(0.814821, "kJ/mol").unwrap()
+        sigma: units::from(3.16555789, "A").unwrap(),
+        epsilon: units::from(0.65017254, "kJ/mol").unwrap()
     });
-    let mut pairs = PairInteraction::new(lj, 14.0);
+    let mut pairs = PairInteraction::new(lj, 6.0);
     // Restrict interactions to act only between different molecules.
     pairs.set_restriction(PairRestriction::InterMolecular);
     pairs.enable_tail_corrections();
-    system.interactions_mut().add_pair("C", "C", pairs);
+    system.interactions_mut().add_pair("O", "O", pairs);
 
     // Add bonds: we use fixed bond lengths assuming
     // the equilibrium bond length. This means that both
     // energy as well as virial for the bond potential are
     // zero. Hence, we use a `NullPotential`.
     let bond = Box::new(NullPotential{});
-    system.interactions_mut().add_bond("C", "C", bond);
+    system.interactions_mut().add_bond("O", "H", bond);
+    let angle = Box::new(NullPotential{});
+    system.interactions_mut().add_angle("H", "O", "H", angle);
+
+    // Set charges
+    let h = 0.42380;
+    let o = - 2.0 * h;
+    for p in &mut system {
+        match p.name() {
+            "H" => p.charge = h,
+            "O" => p.charge = o,
+            _ => panic!("Particle {}", p.name()),
+        }
+    }
+
+    // Add ewald summation method.
+    let ewald = Ewald::new(6.0, 7);
+    system.interactions_mut().set_coulomb(Box::new(ewald));
 
     // Check if bonds are guessed correctly.
-    assert_eq!(system.size() as f64 / system.molecules().len() as f64, 2.0);
+    assert_eq!(system.size() as f64 / system.molecules().len() as f64, 3.0);
 
     system
 }
@@ -52,8 +68,8 @@ fn main() {
     START.call_once(|| {Logger::stdout();});
     let mut system = get_system();
     
-    let temperature = units::from(217.0, "K").unwrap();
-    let pressure = units::from(5.98, "bar").unwrap();
+    let temperature = units::from(300.0, "K").unwrap();
+    let pressure = units::from(1.0, "bar").unwrap();
 
     let mut mc = MonteCarlo::new(temperature);
 
@@ -72,9 +88,9 @@ fn main() {
     // since the system has so few particles that almost all moves are
     // accepted no matter what the amplitude will be.
     mc.add_move_with_acceptance(
-        Box::new(Translate::new(delta_trans)), 50.0, 0.5);
+        Box::new(Translate::new(delta_trans)), 45.0, 0.5);
     mc.add_move_with_acceptance(
-        Box::new(Rotate::new(delta_rot)), 50.0, 0.5);        
+        Box::new(Rotate::new(delta_rot)), 45.0, 0.5);        
     mc.add_move_with_acceptance(
         Box::new(Resize::new(pressure, delta_vol)), 2.0, 0.5);    
     mc.set_amplitude_update_frequency(500);
@@ -84,21 +100,23 @@ fn main() {
     
     // Add output.
     simulation.add_output_with_frequency(
-        Box::new(PropertiesOutput::new("npt_ethane_prp.dat").unwrap()), 500);
+        Box::new(PropertiesOutput::new("npt_spce_prp.dat").unwrap()), 500);
     simulation.add_output_with_frequency(
-        Box::new(EnergyOutput::new("npt_ethane_ener.dat").unwrap()), 500);
+        Box::new(EnergyOutput::new("npt_spce_ener.dat").unwrap()), 500);
     simulation.add_output_with_frequency(
-        Box::new(TrajectoryOutput::new("npt_ethane_conf.xyz").unwrap()), 10000);
+        Box::new(TrajectoryOutput::new("npt_spce_conf.xyz").unwrap()), 10000);
 
     // Often, a simulation is described using `MC cycles`.
     // We define a `cycle` to contain `nmols+2` moves.
     // (See frequencies of moves:
     // nmols/2 translations + nmols/2 rotations + 2 resize moves)
-    let moves_per_cycle = 102;
+    let moves_per_cycle = 92;
     let cycles = 10000;
 
     // Some output and start of the simulation.
-    println!("Simuation of 100 ethane molecules.");
+    println!("Simuation of 90 spce molecules.");
+    let ener_init = units::to(system.total_energy(), "kJ/mol").unwrap();
+    println!("Initial energy: {}", ener_init);
     println!("Simulating {} cycles with a total of {} moves", 
         cycles, cycles * moves_per_cycle);
     println!("  running ....");
