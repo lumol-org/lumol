27f0d94bea5685a2f843aea1479edec9c757ba2a
antoinewdg <antoinewendlinger@gmail.com>
2017-02-09 14:04:32 +0100

    Rewrite mc-water integration test

--- tests/mc-water.rs
+++ tests/mc-water.rs
@@ -4,54 +4,32 @@ extern crate lumol;
 extern crate lumol_input as input;
 
 use lumol::Logger;
-use lumol::sys::{UnitCell, System, Trajectory};
-use lumol::units;
-use lumol::sim::Simulation;
-use lumol::sim::mc::{MonteCarlo, Translate, Rotate};
-
-use input::InteractionsInput;
+use input::Input;
 
 use std::path::Path;
 use std::sync::{Once, ONCE_INIT};
 static START: Once = ONCE_INIT;
 
-fn get_system(potential: &str) -> System {
-    let data_dir = Path::new(file!()).parent().unwrap().join("data");
-    let configuration = data_dir.join("water.xyz");
-    let mut system = Trajectory::open(configuration)
-                                .and_then(|mut traj| traj.read_guess_bonds())
-                                .unwrap();
-    system.set_cell(UnitCell::cubic(18.0));
 
-    let input = InteractionsInput::new(data_dir.join(potential)).unwrap();
-    input.read(&mut system).unwrap();
-    return system;
-}
 
 // This test only run a Monte-Carlo simulation of water, but do not test
 // anything for now. It should test the g(r) function someday.
 #[test]
 fn wolf() {
     START.call_once(|| {Logger::stdout();});
-    let mut system = get_system("water-wolf.toml");
+    let path = Path::new(file!()).parent().unwrap().join("data")
+                                 .join("mc-water-wolf.toml");
 
-    let mut mc = MonteCarlo::new(units::from(300.0, "K").unwrap());
-    mc.add(Box::new(Translate::new(units::from(3.0, "A").unwrap())), 1.0);
-    mc.add(Box::new(Rotate::new(units::from(20.0, "deg").unwrap())), 1.0);
-    let mut simulation = Simulation::new(Box::new(mc));
-
-    simulation.run(&mut system, 5000);
+    let mut config = Input::new(path).unwrap().read().unwrap();
+    config.simulation.run(&mut config.system, config.nsteps);
 }
 
 #[test]
 fn ewald() {
     START.call_once(|| {Logger::stdout();});
-    let mut system = get_system("water-ewald.toml");
+    let path = Path::new(file!()).parent().unwrap().join("data")
+                                 .join("mc-water-ewald.toml");
 
-    let mut mc = MonteCarlo::new(units::from(300.0, "K").unwrap());
-    mc.add(Box::new(Translate::new(units::from(3.0, "A").unwrap())), 1.0);
-    mc.add(Box::new(Rotate::new(units::from(20.0, "deg").unwrap())), 1.0);
-    let mut simulation = Simulation::new(Box::new(mc));
-
-    simulation.run(&mut system, 1000);
+    let mut config = Input::new(path).unwrap().read().unwrap();
+    config.simulation.run(&mut config.system, config.nsteps);
 }
