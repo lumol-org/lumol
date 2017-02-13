1fb11b3b6e8decf598477370371e84d2f2a795cb
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2017-02-13 15:06:33 +0100

    Add Born-Mayer-Huggins pair potential

--- src/core/src/energy/functions.rs
+++ src/core/src/energy/functions.rs
@@ -248,6 +248,72 @@ impl Potential for Torsion {
 impl DihedralPotential for Torsion {}
 
 
+/// Born-Mayer-Huggins potential.
+///
+/// The following potential expression is used: `V(x) = A * exp((sigma - r) /
+/// rho) - C/r^6 + D/r^8`; where `A`, `C` and `D` are energetic constants;
+/// `sigma` and `rho` are length parameters.
+///
+/// # Examples
+///
+/// ```
+/// use lumol::energy::Potential;
+/// use lumol::energy::BornMayerHuggins;
+///
+/// let potential = BornMayerHuggins{a: 2.0, c: 1.0, d: 0.5, sigma: 1.5, rho: 5.3};
+/// assert_eq!(potential.energy(2.2), 1.7446409593340713);
+/// assert_eq!(potential.force(2.2), 0.30992873382584607);
+/// ```
+#[derive(Clone, Copy)]
+pub struct BornMayerHuggins {
+    /// Exponential term energetic constant
+    pub a: f64,
+    /// `1/r^6` term energetic constant
+    pub c: f64,
+    /// `1/r^8` term energetic constant
+    pub d: f64,
+    /// Sphere diameter length constant
+    pub sigma: f64,
+    /// Width of the exponential term length constant
+    pub rho: f64,
+}
+
+impl Potential for BornMayerHuggins {
+    #[inline]
+    fn energy(&self, r: f64) -> f64 {
+        let r2 = r * r;
+        let r6 = r2 * r2 * r2;
+        let exp = f64::exp((self.sigma - r) / self.rho);
+        self.a * exp - self.c / r6 + self.d / (r6 * r2)
+    }
+
+    #[inline]
+    fn force(&self, r: f64) -> f64 {
+        let r2 = r * r;
+        let r7 = r2 * r2 * r2 * r;
+        let exp = f64::exp((self.sigma - r) / self.rho);
+        self.a / self.rho * exp - 6.0 * self.c / r7 + 8.0 * self.d / (r7 * r2)
+    }
+}
+
+impl PairPotential for BornMayerHuggins {
+    fn tail_energy(&self, rc: f64) -> f64 {
+        let rc2 = rc * rc;
+        let rc3 = rc2 * rc;
+        let exp = f64::exp((self.sigma - rc) / self.rho);
+        let factor = rc2 - 2.0 * rc * self.rho + 2.0 * self.rho * self.rho;
+        self.a * self.rho * exp * factor - self.c / (3.0 * rc3) + self.d / (5.0 * rc2 * rc3)
+    }
+
+    fn tail_virial(&self, rc: f64) -> f64 {
+        let rc2 = rc * rc;
+        let rc3 = rc2 * rc;
+        let exp = f64::exp((self.sigma - rc) / self.rho);
+        let factor = rc3 + 3.0 * rc2 * self.rho + 6.0 * rc * self.rho * self.rho + 6.0 * self.rho * self.rho * self.rho;
+        self.a * exp * factor - 20.0 * self.c / rc3 + 8.0 * self.d / (5.0 * rc2 * rc3)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -338,5 +404,21 @@ mod tests {
         let e0 = torsion.energy(4.0);
         let e1 = torsion.energy(4.0 + EPS);
         assert_approx_eq!((e0 - e1)/EPS, torsion.force(4.0), 1e-6);
+    }
+
+    #[test]
+    fn born() {
+        let born = BornMayerHuggins{a: 2.0, c: 1.0, d: 0.5, sigma: 2.0, rho: 2.0};
+
+        // Comparing to externally computed values
+        assert_eq!(born.energy(2.0), 1.986328125);
+        assert_eq!(born.force(2.0), 0.9609375);
+
+        assert_eq!(born.tail_energy(10.0), 4.981521444402363);
+        assert_eq!(born.tail_virial(10.0), 69.13986044386026);
+
+        let e0 = born.energy(4.0);
+        let e1 = born.energy(4.0 + EPS);
+        assert_approx_eq!((e0 - e1)/EPS, born.force(4.0), 1e-6);
     }
 }
