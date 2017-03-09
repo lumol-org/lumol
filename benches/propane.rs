2bd61f49c61f650196abf7d6cbfedc1d79a6c984
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2017-03-09 17:52:39 +0100

    Add a benchmark for propane

---
+++ benches/propane.rs
@@ -0,0 +1,35 @@
+// Lumol, an extensible molecular simulation engine
+// Copyright (C) 2015-2016 Lumol's contributors — BSD license
+
+#[macro_use]
+extern crate bencher;
+extern crate lumol;
+extern crate lumol_input;
+
+use bencher::Bencher;
+mod utils;
+
+
+fn energy(bencher: &mut Bencher) {
+    let system = utils::get_system("propane");
+    bencher.iter(||{
+        let _ = system.potential_energy();
+    })
+}
+
+fn forces(bencher: &mut Bencher) {
+    let system = utils::get_system("propane");
+    bencher.iter(||{
+        let _ = system.forces();
+    })
+}
+
+fn virial(bencher: &mut Bencher) {
+    let system = utils::get_system("propane");
+    bencher.iter(||{
+        let _ = system.virial();
+    })
+}
+
+benchmark_group!(propane, energy, forces, virial);
+benchmark_main!(propane);
