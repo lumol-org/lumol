aef34ba177a170deac39ab15dee3b4767722515f
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2017-03-09 17:29:56 +0100

    Fix a bug in the molecule merging code
    We need to store whether the particles were in the same molecule
    before merging them, not after ...

--- src/core/src/sys/chfl.rs
+++ src/core/src/sys/chfl.rs
@@ -308,24 +308,48 @@ mod tests {
     use super::*;
     use std::io::prelude::*;
     use sys::molecule_type;
+    use sys::{Bond, Angle};
 
-    static MOLECULE: &'static str = "3
+    static WATER: &'static str = "3
 
 O 0.0 0.0 0.0
 H 1.0 0.0 0.0
 H 0.0 1.0 0.0
 ";
 
+    static PROPANE: &'static str = "11
+
+H     -1.306761     0.917434     0.885782
+C     -1.277333     0.278101     0.000000
+H     -2.172669    -0.348524    -0.000126
+H     -1.306638     0.917616    -0.885655
+C      0.000000    -0.596258     0.000000
+H      0.000000    -1.247870    -0.879715
+H      0.000000    -1.247870     0.879715
+C      1.277333     0.278101     0.000000
+H      1.306675     0.917562     0.885693
+H      1.306724     0.917489    -0.885744
+H      2.172669     -0.348524    0.000051
+";
+
     #[test]
-    fn read() {
+    fn read_water() {
         let mut file = NamedTempFileOptions::new().suffix(".xyz").create().unwrap();
-        write!(file, "{}", MOLECULE).unwrap();
+        write!(file, "{}", WATER).unwrap();
 
         let (molecule, atoms) = read_molecule(file.path()).unwrap();
 
         assert_eq!(molecule.size(), 3);
         assert_eq!(molecule.size(), atoms.len());
+
         assert_eq!(molecule.bonds().len(), 2);
+        assert!(molecule.bonds().contains(&Bond::new(0, 1)));
+        assert!(molecule.bonds().contains(&Bond::new(0, 2)));
+
+        assert_eq!(molecule.angles().len(), 1);
+        assert!(molecule.angles().contains(&Angle::new(1, 0, 2)));
+
+        assert!(molecule.dihedrals().is_empty());
 
         assert_eq!(atoms[0].name(), "O");
         assert_eq!(atoms[1].name(), "H");
@@ -334,5 +358,24 @@ H 0.0 1.0 0.0
         // This is only a simple regression test on the moltype function. Feel
         // free to change the value if the molecule type algorithm change.
         assert_eq!(molecule_type(&molecule, &atoms), 2727145596042757306);
+    }
+
+    #[test]
+    fn read_propane() {
+        let mut file = NamedTempFileOptions::new().suffix(".xyz").create().unwrap();
+        write!(file, "{}", PROPANE).unwrap();
+
+        let (molecule, atoms) = read_molecule(file.path()).unwrap();
+
+        assert_eq!(molecule.size(), 11);
+        assert_eq!(molecule.size(), atoms.len());
+
+        assert_eq!(molecule.bonds().len(), 10);
+        assert_eq!(molecule.angles().len(), 18);
+        assert_eq!(molecule.dihedrals().len(), 18);
+
+        // This is only a simple regression test on the moltype function. Feel
+        // free to change the value if the molecule type algorithm change.
+        assert_eq!(molecule_type(&molecule, &atoms), 2028056351119909064);
     }
 }
