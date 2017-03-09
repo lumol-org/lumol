81510d1cdc0e1a3b1af6064a40a7ed9db1d8a9d0
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2017-03-09 15:05:30 +0100

    Add benchmarks for Wolf summation

--- benches/nacl.rs
+++ benches/nacl.rs
@@ -8,7 +8,7 @@ extern crate lumol;
 use bencher::Bencher;
 
 use lumol::Logger;
-use lumol::energy::{Ewald, GlobalPotential};
+use lumol::energy::{Ewald, Wolf, GlobalPotential};
 
 use std::sync::{Once, ONCE_INIT};
 pub static START: Once = ONCE_INIT;
@@ -19,7 +19,7 @@ mod utils;
 fn energy_ewald(bencher: &mut Bencher) {
     START.call_once(|| {Logger::stdout();});
     let system = utils::get_system("nacl");
-    let mut ewald = Ewald::new(5.5, 7);
+    let mut ewald = Ewald::new(9.5, 7);
 
     bencher.iter(||{
         let _ = ewald.energy(&system);
@@ -29,7 +29,7 @@ fn energy_ewald(bencher: &mut Bencher) {
 fn forces_ewald(bencher: &mut Bencher) {
     START.call_once(|| {Logger::stdout();});
     let system = utils::get_system("nacl");
-    let mut ewald = Ewald::new(5.5, 7);
+    let mut ewald = Ewald::new(9.5, 7);
 
     bencher.iter(||{
         let _ = ewald.forces(&system);
@@ -39,12 +39,43 @@ fn forces_ewald(bencher: &mut Bencher) {
 fn virial_ewald(bencher: &mut Bencher) {
     START.call_once(|| {Logger::stdout();});
     let system = utils::get_system("nacl");
-    let mut ewald = Ewald::new(5.5, 7);
+    let mut ewald = Ewald::new(9.5, 7);
 
     bencher.iter(||{
         let _ = ewald.virial(&system);
     })
 }
 
+fn energy_wolf(bencher: &mut Bencher) {
+    START.call_once(|| {Logger::stdout();});
+    let system = utils::get_system("nacl");
+    let mut wolf = Wolf::new(12.0);
+
+    bencher.iter(||{
+        let _ = wolf.energy(&system);
+    })
+}
+
+fn forces_wolf(bencher: &mut Bencher) {
+    START.call_once(|| {Logger::stdout();});
+    let system = utils::get_system("nacl");
+    let mut wolf = Wolf::new(12.0);
+
+    bencher.iter(||{
+        let _ = wolf.forces(&system);
+    })
+}
+
+fn virial_wolf(bencher: &mut Bencher) {
+    START.call_once(|| {Logger::stdout();});
+    let system = utils::get_system("nacl");
+    let mut wolf = Wolf::new(12.0);
+
+    bencher.iter(||{
+        let _ = wolf.virial(&system);
+    })
+}
+
 benchmark_group!(ewald, energy_ewald, forces_ewald, virial_ewald);
-benchmark_main!(ewald);
+benchmark_group!(wolf, energy_wolf, forces_wolf, virial_wolf);
+benchmark_main!(ewald, wolf);
--- benches/water.rs
+++ benches/water.rs
@@ -8,7 +8,7 @@ extern crate lumol;
 use bencher::Bencher;
 
 use lumol::Logger;
-use lumol::energy::{Ewald, PairRestriction, CoulombicPotential, GlobalPotential};
+use lumol::energy::{Ewald, Wolf, PairRestriction, CoulombicPotential, GlobalPotential};
 
 use std::sync::{Once, ONCE_INIT};
 pub static START: Once = ONCE_INIT;
@@ -16,9 +16,15 @@ pub static START: Once = ONCE_INIT;
 mod utils;
 
 fn get_ewald() -> Ewald {
-    let mut ewald = Ewald::new(9.0, 7);
+    let mut ewald = Ewald::new(8.0, 7);
     ewald.set_restriction(PairRestriction::InterMolecular);
     ewald
+}
+
+fn get_wolf() -> Wolf {
+    let mut wolf = Wolf::new(9.0);
+    wolf.set_restriction(PairRestriction::InterMolecular);
+    wolf
 }
 
 fn energy_ewald(bencher: &mut Bencher) {
@@ -51,5 +57,36 @@ fn virial_ewald(bencher: &mut Bencher) {
     })
 }
 
+fn energy_wolf(bencher: &mut Bencher) {
+    START.call_once(|| {Logger::stdout();});
+    let system = utils::get_system("water");
+    let mut wolf = get_wolf();
+
+    bencher.iter(||{
+        let _ = wolf.energy(&system);
+    })
+}
+
+fn forces_wolf(bencher: &mut Bencher) {
+    START.call_once(|| {Logger::stdout();});
+    let system = utils::get_system("water");
+    let mut wolf = get_wolf();
+
+    bencher.iter(||{
+        let _ = wolf.forces(&system);
+    })
+}
+
+fn virial_wolf(bencher: &mut Bencher) {
+    START.call_once(|| {Logger::stdout();});
+    let system = utils::get_system("water");
+    let mut wolf = get_wolf();
+
+    bencher.iter(||{
+        let _ = wolf.virial(&system);
+    })
+}
+
 benchmark_group!(ewald, energy_ewald, forces_ewald, virial_ewald);
-benchmark_main!(ewald);
+benchmark_group!(wolf, energy_wolf, forces_wolf, virial_wolf);
+benchmark_main!(ewald, wolf);
