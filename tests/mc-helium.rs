431fca200fcb3d62c23f216f3dc05c98f95ef25b
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2017-03-16 21:53:29 +0100

    Reorganise tests data files
    The integration tests now have one folder by test containing all the data files.
    This means that we have some files duplicated, but it makes navigating the data files
    easier.

---
+++ tests/mc-helium.rs
@@ -0,0 +1,38 @@
+// Lumol, an extensible molecular simulation engine
+// Copyright (C) 2015-2016 Lumol's contributors — BSD license
+
+//! Testing physical properties of a Lennard-Jones gaz of Helium using
+//! Monte-Carlo simulation
+extern crate lumol;
+extern crate lumol_input as input;
+
+use lumol::Logger;
+use lumol::units;
+use lumol::consts::K_BOLTZMANN;
+
+use input::Input;
+
+use std::path::Path;
+use std::sync::{Once, ONCE_INIT};
+static START: Once = ONCE_INIT;
+
+
+#[test]
+fn perfect_gas() {
+    START.call_once(|| {Logger::stdout();});
+    let path = Path::new(file!()).parent().unwrap()
+                                 .join("data")
+                                 .join("mc-helium")
+                                 .join("nvt.toml");
+
+    let mut config = Input::new(path).unwrap().read().unwrap();
+    config.simulation.run(&mut config.system, config.nsteps);
+    let pressure = config.system.pressure();
+    let volume = config.system.volume();
+    let temperature = units::from(300.0, "K").unwrap();
+
+    let pv = pressure * volume;
+    let nkt = config.system.size() as f64 * K_BOLTZMANN * temperature;
+    let msg = format!("{} {}", f64::abs(pv - nkt), f64::abs(pv - nkt) / pv);
+    assert!(f64::abs(pv - nkt) / pv < 2e-2, msg);
+}
---
+++ tests/md-butane.rs
@@ -0,0 +1,47 @@
+// Lumol, an extensible molecular simulation engine
+// Copyright (C) 2015-2016 Lumol's contributors — BSD license
+
+//! Testing molecular dynamics of butane
+extern crate lumol;
+extern crate lumol_input as input;
+
+use lumol::Logger;
+use input::Input;
+
+use std::path::Path;
+use std::sync::{Once, ONCE_INIT};
+static START: Once = ONCE_INIT;
+
+
+#[test]
+fn bonds_detection() {
+    START.call_once(|| {Logger::stdout();});
+    let path = Path::new(file!()).parent().unwrap()
+                                 .join("data")
+                                 .join("md-butane")
+                                 .join("nve.toml");
+    let system = Input::new(path).unwrap().read_system().unwrap();
+    assert_eq!(system.molecules().len(), 50);
+
+    for molecule in system.molecules() {
+        assert_eq!(molecule.bonds().len(), 3);
+        assert_eq!(molecule.angles().len(), 2);
+        assert_eq!(molecule.dihedrals().len(), 1);
+    }
+}
+
+#[test]
+fn constant_energy() {
+    START.call_once(|| {Logger::stdout();});
+    let path = Path::new(file!()).parent().unwrap()
+                                 .join("data")
+                                 .join("md-butane")
+                                 .join("nve.toml");
+    let mut config = Input::new(path).unwrap().read().unwrap();
+
+
+    let e_initial = config.system.total_energy();
+    config.simulation.run(&mut config.system, config.nsteps);
+    let e_final = config.system.total_energy();
+    assert!(f64::abs((e_initial - e_final)/e_final) < 1e-3);
+}
