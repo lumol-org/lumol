17bea20abaa94b48df1823fcdd69d843673570c7
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2017-03-16 21:53:29 +0100

    Reorganise tests data files
    The integration tests now have one folder by test containing all the data files.
    This means that we have some files duplicated, but it makes navigating the data files
    easier.

---
+++ tests/mc-water.rs
@@ -0,0 +1,38 @@
+// Lumol, an extensible molecular simulation engine
+// Copyright (C) 2015-2016 Lumol's contributors — BSD license
+extern crate lumol;
+extern crate lumol_input as input;
+
+use lumol::Logger;
+use input::Input;
+
+use std::path::Path;
+use std::sync::{Once, ONCE_INIT};
+static START: Once = ONCE_INIT;
+
+
+// This test only run a Monte-Carlo simulation of water, but do not test
+// anything for now. It should test the g(r) function someday.
+#[test]
+fn wolf() {
+    START.call_once(|| {Logger::stdout();});
+    let path = Path::new(file!()).parent().unwrap()
+                                 .join("data")
+                                 .join("mc-water")
+                                 .join("nvt-wolf.toml");
+
+    let mut config = Input::new(path).unwrap().read().unwrap();
+    config.simulation.run(&mut config.system, config.nsteps);
+}
+
+#[test]
+fn ewald() {
+    START.call_once(|| {Logger::stdout();});
+    let path = Path::new(file!()).parent().unwrap()
+                                 .join("data")
+                                 .join("mc-water")
+                                 .join("nvt-ewald.toml");
+
+    let mut config = Input::new(path).unwrap().read().unwrap();
+    config.simulation.run(&mut config.system, config.nsteps);
+}
---
+++ tests/md-helium.rs
@@ -0,0 +1,142 @@
+// Lumol, an extensible molecular simulation engine
+// Copyright (C) 2015-2016 Lumol's contributors — BSD license
+
+//! Testing physical properties of a Lennard-Jones gas of Helium using Molecular
+//! dynamics
+extern crate lumol;
+extern crate lumol_input as input;
+
+use lumol::Logger;
+use lumol::consts::K_BOLTZMANN;
+use lumol::units;
+
+use input::Input;
+
+use std::path::Path;
+use std::sync::{Once, ONCE_INIT};
+static START: Once = ONCE_INIT;
+
+mod utils;
+
+
+#[test]
+fn constant_energy_velocity_verlet() {
+    START.call_once(|| {Logger::stdout();});
+    let path = Path::new(file!()).parent().unwrap()
+                                 .join("data")
+                                 .join("md-helium")
+                                 .join("nve-velocity-verlet.toml");
+    let mut config = Input::new(path).unwrap().read().unwrap();
+
+    let e_initial = config.system.total_energy();
+    config.simulation.run(&mut config.system, config.nsteps);
+    let e_final = config.system.total_energy();
+    assert!(f64::abs((e_initial - e_final)/e_final) < 1e-3);
+}
+
+
+#[test]
+fn constant_energy_verlet() {
+    START.call_once(|| {Logger::stdout();});
+let path = Path::new(file!()).parent().unwrap()
+                             .join("data")
+                             .join("md-helium")
+                             .join("nve-verlet.toml");
+    let mut config = Input::new(path).unwrap().read().unwrap();
+
+    let e_initial = config.system.total_energy();
+    config.simulation.run(&mut config.system, config.nsteps);
+    let e_final = config.system.total_energy();
+    assert!(f64::abs((e_initial - e_final)/e_final) < 1e-2);
+}
+
+
+#[test]
+fn constant_energy_leap_frog() {
+    START.call_once(|| {Logger::stdout();});
+    let path = Path::new(file!()).parent().unwrap()
+                                 .join("data")
+                                 .join("md-helium")
+                                 .join("nve-leap-frog.toml");
+    let mut config = Input::new(path).unwrap().read().unwrap();
+
+    let e_initial = config.system.total_energy();
+    config.simulation.run(&mut config.system, config.nsteps);
+    let e_final = config.system.total_energy();
+    assert!(f64::abs((e_initial - e_final)/e_final) < 1e-3);
+}
+
+#[test]
+fn perfect_gas() {
+    START.call_once(|| {Logger::stdout();});
+    let path = Path::new(file!()).parent().unwrap()
+                                 .join("data")
+                                 .join("md-helium")
+                                 .join("nve-perfect-gas.toml");
+    let mut config = Input::new(path).unwrap().read().unwrap();
+
+    config.simulation.run(&mut config.system, config.nsteps);
+
+    let pressure = config.system.pressure();
+    let volume = config.system.volume();
+    let temperature = config.system.temperature();
+    let n = config.system.size() as f64;
+
+    assert!(f64::abs(pressure * volume - n * K_BOLTZMANN * temperature) < 1e-3);
+}
+
+#[test]
+fn berendsen_barostat() {
+    START.call_once(|| {Logger::stdout();});
+    let path = Path::new(file!()).parent().unwrap()
+                                 .join("data")
+                                 .join("md-helium")
+                                 .join("npt-berendsen-barostat.toml");
+    let mut config = Input::new(path).unwrap().read().unwrap();
+
+    let collecter = utils::Collecter::new(4000);
+    let temperatures = collecter.temperatures();
+    let pressures = collecter.pressures();
+
+    config.simulation.add_output(Box::new(collecter));
+    config.simulation.run(&mut config.system, config.nsteps);
+
+    let expected = units::from(5000.0, "bar").unwrap();
+    let pressure = ::utils::mean(pressures.clone());
+    assert!(f64::abs(pressure - expected) / expected < 5e-2);
+
+    let expected = units::from(273.0, "K").unwrap();
+    let temperature = ::utils::mean(temperatures.clone());
+    assert!(f64::abs(temperature - expected) / expected < 1e-2);
+}
+
+#[test]
+fn shifted() {
+    START.call_once(|| {Logger::stdout();});
+    let path = Path::new(file!()).parent().unwrap()
+                                 .join("data")
+                                 .join("md-helium")
+                                 .join("nve-shifted.toml");
+    let mut config = Input::new(path).unwrap().read().unwrap();
+
+    let e_initial = config.system.total_energy();
+    config.simulation.run(&mut config.system, config.nsteps);
+    let e_final = config.system.total_energy();
+    assert!(f64::abs((e_initial - e_final)/e_final) < 2e-3);
+}
+
+
+#[test]
+fn table_computation() {
+    START.call_once(|| {Logger::stdout();});
+    let path = Path::new(file!()).parent().unwrap()
+                                 .join("data")
+                                 .join("md-helium")
+                                 .join("nve-table.toml");
+    let mut config = Input::new(path).unwrap().read().unwrap();
+
+    let e_initial = config.system.total_energy();
+    config.simulation.run(&mut config.system, config.nsteps);
+    let e_final = config.system.total_energy();
+    assert!(f64::abs((e_initial - e_final)/e_final) < 2e-3);
+}
