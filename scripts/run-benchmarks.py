4be925fdf4a5e512dd8bd30934267c1a9759b661
antoinewdg <antoinewendlinger@gmail.com>
2017-03-28 11:15:03 +0200

    Implement PR comments
    @bot benchmark 2

--- scripts/run-benchmarks.py
+++ scripts/run-benchmarks.py
@@ -24,6 +24,8 @@ COMPARISON_TEMPLATE = """
 </p></details>
 """
 
+BASE_REPO_URL = 'https://github.com/lumol-org/lumol.git'
+
 
 def get_pull_request_id():
     pr_id = os.environ.get('TRAVIS_PULL_REQUEST', 'false')
@@ -34,8 +36,8 @@ def get_pull_request_id():
 
 def request_api(endpoint, data=None):
     url = 'https://api.github.com/repos/lumol-org/lumol' + endpoint
-    username = os.environ['GITHUB_USER_NAME']
-    token = os.environ['GITHUB_USER_TOKEN']
+    username = os.environ['LUMOL_BOT_GH_USERNAME']
+    token = os.environ['LUMOL_BOT_GH_TOKEN']
 
     cmd = "curl -sS %s  -u '%s:%s'" % (url, username, token)
     if data is not None:
@@ -46,7 +48,7 @@ def request_api(endpoint, data=None):
 
 
 def check_if_benches_should_run():
-    EXPR = '@bot benchmark'
+    EXPR = '[lumol bench '
     pr_id = get_pull_request_id()
     if pr_id is None:
         print("This is not a PR, the benchmarks are not run.")
@@ -54,7 +56,6 @@ def check_if_benches_should_run():
 
     # Get the text of the PR in `body`
     out = request_api('/pulls/%s' % pr_id)
-
     clone_url = out['head']['repo']['clone_url']
     body = out['body']
 
@@ -66,7 +67,7 @@ def check_if_benches_should_run():
     end = len(body)
     try:
         s = begin + len(EXPR) + 1
-        end = body[s:].index(' ') + s
+        end = body[s:].index(']') + s
     except ValueError:
         pass
 
@@ -75,15 +76,39 @@ def check_if_benches_should_run():
     return int(n_commits), clone_url
 
 
+def get_master_commit():
+    cmd = 'git log --oneline upstream/master -n 1'
+    out = subprocess.check_output(cmd, shell=True).decode('utf-8')
+    h, _, title = out.rstrip().partition(' ')
+    return h, title
+
+
 def get_commit_descriptions(n_commits):
+    """
+    Get hash and title of the `n_commits` latest commits on the branch.
+
+    Also adds the commit at the HEAD of master in the end. If this
+    commit is met earlier, stops at this commit (the master commit
+    is guaranteed to be at the end of the result.
+
+    :param n_commits:
+    :return:
+    """
     cmd = 'git log --oneline | head -n %s' % n_commits
     out = subprocess.check_output(cmd, shell=True).decode('utf-8')
+    master_h, master_title = get_master_commit()
+
     descriptions = []
     for line in out.split('\n'):
         if line.rstrip() == '':
             continue
         h, _, title = line.partition(' ')
         descriptions.append((h, title))
+        if h == master_h:
+            break
+
+    if descriptions[-1][0] != master_h:
+        descriptions.append((master_h, master_title))
 
     return descriptions
 
@@ -100,11 +125,11 @@ def run_bench(k):
     subprocess.call('cargo bench > ../bench_%s.txt' % k, shell=True)
 
 
-def run_benches_for_n_commits(n_commits):
-    for k in range(n_commits):
-        print('=================== Benching commit %s ==============================' % k)
-        run_bench(k)
-        subprocess.call('git checkout HEAD~', shell=True)
+def run_benches_for_n_commits(description):
+    for h, title in description:
+        print('=================== Benching commit %s ==============================' % h)
+        subprocess.call('git checkout %s' % h, shell=True)
+        run_bench(h)
         print('=================== Done ==============================')
 
 
@@ -114,17 +139,18 @@ def cd_to_cloned_repo(url):
     subprocess.call('pwd', shell=True)
     subprocess.call('git checkout %s' % os.environ['TRAVIS_PULL_REQUEST_BRANCH'], shell=True)
     subprocess.call('git status', shell=True)
+    subprocess.call('git remote add upstream %s' % BASE_REPO_URL, shell=True)
+    subprocess.call('git fetch upstream master', shell=True)
 
 
-def comment_pull_request(n_commits):
+def comment_pull_request(descriptions):
     pr_id = get_pull_request_id()
-    descriptions = get_commit_descriptions(n_commits)
 
     # Comparison benchmarks
-    comment = '## Comparison benchmarks\n (comparisons with %s)' % descriptions[-1][0]
+    comment = '## Comparing to master (%s)\nusing `--threshold 2`' % descriptions[-1][0]
 
     for k, (h, title) in enumerate(descriptions[:-1]):
-        cmd = 'cargo benchcmp ../bench_%s.txt ../bench_%s' % (n_commits - 1, k)
+        cmd = 'cargo benchcmp ../bench_%s.txt ../bench_%s.txt --threshold 2' % (descriptions[-1][0], h)
         out = subprocess.check_output(cmd, shell=True).decode('utf-8')
         comment += COMPARISON_TEMPLATE % (h, title, out)
 
@@ -132,7 +158,7 @@ def comment_pull_request(n_commits):
     comment += '\n## Individual benchmarks\n'
 
     for k, (h, title) in enumerate(descriptions):
-        with open('../bench_%s.txt' % k) as f:
+        with open('../bench_%s.txt' % h) as f:
             bench = f.read()
         comment += INDIVIDUAL_BENCH_TEMPLATE % (h, title, bench)
 
@@ -149,10 +175,12 @@ def main():
         return
     n_commits, clone_url = result
     cd_to_cloned_repo(clone_url)
+    descriptions = get_commit_descriptions(n_commits)
     run_warmup()
-    run_benches_for_n_commits(n_commits)
-    comment_pull_request(n_commits)
+    run_benches_for_n_commits(descriptions)
+    comment_pull_request(descriptions)
     subprocess.call('rm ../bench_*.txt', shell=True)
+    subprocess.call('rm -rf ../cloned_repo', shell=True)
 
 
 if __name__ == '__main__':
