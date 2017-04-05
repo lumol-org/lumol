28d840b6c80427948cb186daa1bd2fc7402ec194
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2017-04-05 14:08:29 +0200

    Refactor the lumol-input tests runner

--- src/input/tests/input.rs
+++ src/input/tests/input.rs
@@ -1,8 +1,8 @@
 // Lumol, an extensible molecular simulation engine
 // Copyright (C) 2015-2016 Lumol's contributors — BSD license
-
 extern crate test;
 extern crate walkdir;
+extern crate env_logger;
 
 extern crate lumol;
 extern crate lumol_input;
@@ -21,6 +21,26 @@ use lumol::sys::System;
 use lumol_input::{InteractionsInput, Input, Error};
 
 fn main() {
+    env_logger::init().expect("could not set logger");
+    let _cleanup = TestsCleanup;
+
+    let args: Vec<_> = env::args().collect();
+    let mut opts = match test::parse_opts(&args).expect("no options") {
+        Ok(opts) => opts,
+        Err(msg) => panic!("{:?}", msg),
+    };
+    opts.verbose = true;
+
+    let tests = all_tests();
+    let result = test::run_tests_console(&opts, tests);
+    match result {
+        Ok(true) => {}
+        Ok(false) => std::process::exit(-1),
+        Err(err) => panic!("io error when running tests: {:?}", err),
+    }
+}
+
+fn all_tests() -> Vec<TestDescAndFn> {
     let mut tests = Vec::new();
 
     tests.extend(generate_tests("simulation/good", |path| {
@@ -66,28 +86,18 @@ fn main() {
         })
     }).expect("Could not generate the tests"));
 
-    let args: Vec<_> = env::args().collect();
-    let mut opts = match test::parse_opts(&args) {
-        Some(Ok(opts)) => opts,
-        Some(Err(msg)) => panic!("{:?}", msg),
-        None => return,
-    };
-    opts.verbose = true;
-
-    let result = test::run_tests_console(&opts, tests);
-    cleanup();
-    match result {
-        Ok(true) => {}
-        Ok(false) => std::process::exit(-1),
-        Err(err) => panic!("io error when running tests: {:?}", err),
-    }
+    return tests;
 }
 
-fn generate_tests<F>(group: &str, callback: F) -> Result<Vec<TestDescAndFn>, io::Error>
+/// Generate the tests by calling `callback` for every TOML files at the given
+/// `root`.
+fn generate_tests<F>(root: &str, callback: F) -> Result<Vec<TestDescAndFn>, io::Error>
     where F: Fn(PathBuf) -> Box<FnMut() + Send> {
     let mut tests = Vec::new();
 
-    let dir = PathBuf::new().join(env!("CARGO_MANIFEST_DIR")).join("tests").join(group);
+    let dir = PathBuf::new().join(env!("CARGO_MANIFEST_DIR"))
+                            .join("tests")
+                            .join(root);
     for entry in WalkDir::new(dir) {
         let entry = try!(entry);
         let file_type = entry.file_type();
@@ -95,8 +105,11 @@ fn generate_tests<F>(group: &str, callback: F) -> Result<Vec<TestDescAndFn>, io:
             if let Some(extension) = entry.path().extension() {
                 if extension == "toml" {
                     let path = entry.path();
-                    let name = String::from(group) + "/" + path.file_name().expect("Missing file name")
-                                                               .to_str().expect("File name is invalid UTF-8");
+                    let name = String::from(root) + "/";
+                    let name = name + path.file_name()
+                                          .expect("Missing file name")
+                                          .to_str()
+                                          .expect("File name is invalid UTF-8");
 
                     let test = TestDescAndFn {
                         desc: TestDesc {
@@ -116,16 +129,20 @@ fn generate_tests<F>(group: &str, callback: F) -> Result<Vec<TestDescAndFn>, io:
     Ok(tests)
 }
 
-fn cleanup() {
-    const REMOVE: &'static [&'static str] = &[
-        "energy.dat", "filename.xyz", "cell.dat", "properties.dat"
-    ];
+/// Cleanup temporary files after the tests
+struct TestsCleanup;
+impl Drop for TestsCleanup {
+    fn drop(&mut self) {
+        const REMOVE: &'static [&'static str] = &[
+            "energy.dat", "filename.xyz", "cell.dat", "properties.dat"
+        ];
 
-    for file in REMOVE {
-        if let Err(err) = fs::remove_file(file) {
-            match err.kind() {
-                io::ErrorKind::NotFound => {},
-                _ => panic!("io error in cleanup code: {}", err)
+        for file in REMOVE {
+            if let Err(err) = fs::remove_file(file) {
+                match err.kind() {
+                    io::ErrorKind::NotFound => {},
+                    _ => panic!("io error in cleanup code: {}", err)
+                }
             }
         }
     }
