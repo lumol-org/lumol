c6b4d3bcd5cae32a6d86bc3c4bf3b85070197ae1
antoinewdg <antoinewendlinger@gmail.com>
2017-04-10 16:37:11 +0200

    Stop cloning the whole repository for benchmarking.
    And implement various fixes.

--- scripts/run-benchmarks.py
+++ scripts/run-benchmarks.py
@@ -1,54 +1,70 @@
 import subprocess
-import json
 import os
-import re
 
 import click
 import cpuinfo
+import requests
 
 BASE_REPO_URL = 'https://github.com/lumol-org/lumol.git'
 
 INDIVIDUAL_BENCH_TEMPLATE = """
-<details><summary>%s %s</summary>
+<details><summary>{sha} {title}</summary>
   <p>\n\n
 \n\n
 ```bash\n
-%s\n\n
+{body}\n\n
 ```\n\n
 
 </p></details>
 """
 
 COMPARISON_TEMPLATE = """
-<details><summary>%s %s</summary>
+<details><summary>{sha} {title}</summary>
   <p>\n\n
 \n\n
 ```bash\n
-%s\n\n
+{body}\n\n
 ```\n\n
 
 </p></details>
 """
 
 
+def _get_environment_variable(name):
+    var = os.environ.get(name, None)
+    if var is None:
+        msg = 'The environment variable {} must be set to access Github API'.format(name)
+        raise Exception(msg)
+
+    return var
+
+
+def check_for_cargo_benchcmp():
+    try:
+        subprocess.check_output('cargo benchcmp --help', shell=True)
+    except subprocess.CalledProcessError as e:
+        raise Exception("cargo-benchcmp is not installed, please install with "
+                        "`cargo install cargo-benchcmp`")
+
+
 def request_api(endpoint, data=None):
     url = 'https://api.github.com/repos/lumol-org/lumol' + endpoint
-    username = os.environ['LUMOL_GH_USERNAME']
-    token = os.environ['LUMOL_GH_TOKEN']
+    username = _get_environment_variable('LUMOL_GH_USERNAME')
+    token = _get_environment_variable('LUMOL_GH_TOKEN')
 
-    cmd = "curl -sS %s  -u '%s:%s'" % (url, username, token)
-    if data is not None:
-        cmd += " --data '%s'" % json.dumps(data)
+    if data is None:
+        r = requests.get(url, auth=(username, token))
+    else:
+        r = requests.post(url, auth=(username, token), json=data)
 
-    out = subprocess.check_output(cmd, shell=True).decode('utf-8')
-    return json.loads(out)
+    return r.json()
 
 
 def get_master_commit():
     cmd = 'git log --oneline upstream/master -n 1'
     out = subprocess.check_output(cmd, shell=True).decode('utf-8')
-    h, _, title = out.rstrip().partition(' ')
-    return h, title
+    sha, _, title = out.rstrip().partition(' ')
+    return sha, title
 
 
 def get_commit_descriptions(n_commits):
@@ -62,35 +78,36 @@ def get_commit_descriptions(n_commits):
     :param n_commits:
     :return:
     """
-    cmd = 'git log --oneline | head -n %s' % n_commits
+    cmd = 'git log --oneline  -n {}'.format(n_commits)
     out = subprocess.check_output(cmd, shell=True).decode('utf-8')
-    master_h, master_title = get_master_commit()
+    master_sha, master_title = get_master_commit()
 
     descriptions = []
     for line in out.split('\n'):
         if line.rstrip() == '':
             continue
-        h, _, title = line.partition(' ')
-        descriptions.append((h, title))
-        if h == master_h:
+        sha, _, title = line.partition(' ')
+        descriptions.append((sha, title))
+        if sha == master_sha:
             break
 
-    if descriptions[-1][0] != master_h:
-        descriptions.append((master_h, master_title))
+    if descriptions[-1][0] != master_sha:
+        descriptions.append((master_sha, master_title))
 
     return descriptions
 
 
-def setup_cloned_repo(pr_id):
-    response = request_api('/pulls/%s' % pr_id)
-    clone_url = response['head']['repo']['clone_url']
-    commit_id = response['head']['sha']
+def clean_repo():
+    subprocess.call('git checkout master', shell=True)
+    subprocess.call('git branch -D _bot_pr', shell=True)
+    subprocess.call('git remote remove _bot_remote', shell=True)
 
-    subprocess.call('git clone %s cloned_repo' % clone_url, shell=True)
-    os.chdir('cloned_repo')
-    subprocess.call('git checkout %s' % commit_id, shell=True)
-    subprocess.call('git remote add upstream %s' % BASE_REPO_URL, shell=True)
-    subprocess.call('git fetch upstream master', shell=True)
+
+def setup_repo(pr_id):
+    clean_repo()
+    subprocess.call('git remote add _bot_remote {}'.format(BASE_REPO_URL), shell=True)
+    subprocess.call('git fetch _bot_remote pull/{}/head:_bot_pr'.format(pr_id), shell=True)
+    subprocess.call('git checkout _bot_pr', shell=True)
 
 
 class Benchmarker:
@@ -103,56 +120,55 @@ class Benchmarker:
         for _ in range(3):
             subprocess.check_output('cargo bench', shell=True)
 
-    def run_bench(self, h):
-        cmd = 'cargo bench > %s/%s.txt' % (self.output_dir, h)
+    def run_bench(self, sha):
+        cmd = 'cargo bench > {}/{}.txt'.format(self.output_dir, sha)
         subprocess.call(cmd, shell=True)
 
     def run_all_benches(self):
-        for h, title in self.commit_descriptions:
-            print('=================== Benching commit %s ==============================' % h)
-            subprocess.call('git checkout %s' % h, shell=True)
-            self.run_bench(h)
+        for sha, title in self.commit_descriptions:
+            print('=================== Benchmarking commit {} =============================='.format(sha))
+            subprocess.call('git checkout {}'.format(sha), shell=True)
+            self.run_bench(sha)
             print('=================== Done ==============================')
 
     def compare_benches(self):
         comparisons = {}
-        master_h, _ = self.commit_descriptions[-1]
-        for h, title in self.commit_descriptions[:-1]:
-            cmd = 'cargo benchcmp %s/%s.txt %s/%s.txt --threshold 2 --variance' % (
-                self.output_dir, master_h, self.output_dir, h
-            )
+        master_sha, _ = self.commit_descriptions[-1]
+        for sha, title in self.commit_descriptions[:-1]:
+            cmd = 'cargo benchcmp {dir}/{sha_1}.txt {dir}/{sha_2}.txt --threshold 2 --variance' \
+                .format(dir=self.output_dir, sha_1=master_sha, sha_2=sha)
             out = subprocess.check_output(cmd, shell=True).decode('utf-8')
-            comparisons[h] = out
+            comparisons[sha] = out
 
         return comparisons
 
     def comment_pr(self, pr_id):
         # Comparison benchmarks
-        master_h, master_title = self.commit_descriptions[-1]
-        comment = '## Comparing to master (%s)\nusing `--threshold 2, latest commit first`' % master_h
+        master_sha, master_title = self.commit_descriptions[-1]
+        comment = '## Comparing to master ({})\nusing `--threshold 2, latest commit first`'.format(master_sha)
 
         comparisons = self.compare_benches()
-        for h, title in self.commit_descriptions[:-1]:
-            compare = comparisons[h]
-            comment += COMPARISON_TEMPLATE % (h, title, compare)
+        for sha, title in self.commit_descriptions[:-1]:
+            compare = comparisons[sha]
+            comment += COMPARISON_TEMPLATE.format(sha=sha, title=title, body=compare)
 
         # Individual benchmarks
         comment += '\n## Individual benchmarks\n'
 
-        for k, (h, title) in enumerate(self.commit_descriptions):
-            with open('%s/%s.txt' % (self.output_dir, h)) as f:
+        for k, (sha, title) in enumerate(self.commit_descriptions):
+            with open('{}/{}.txt'.format(self.output_dir, sha)) as f:
                 bench = f.read()
-            comment += INDIVIDUAL_BENCH_TEMPLATE % (h, title, bench)
+            comment += INDIVIDUAL_BENCH_TEMPLATE.format(sha=sha, title=title, body=bench)
 
         info = cpuinfo.get_cpu_info()
         if info is not None:
-            comment += '\n<br>**CPU**: %s' % info['brand']
+            comment += '\n<br>**CPU**: {}'.format(info['brand'])
 
         # Emit the request
         data = {
             'body': comment
         }
-        request_api('/issues/%s/comments' % pr_id, data)
+        request_api('/issues/{}/comments'.format(pr_id), data)
 
 
 @click.command()
@@ -169,16 +185,15 @@ def main(output_dir, n_commits, pr_id):
     and LUMOL_GH_TOKEN to contain respectively the Github username
     and a personal access token.
     """
-
-    setup_cloned_repo(pr_id)
+    check_for_cargo_benchcmp()
+    setup_repo(pr_id)
 
     benchmarker = Benchmarker(n_commits, output_dir)
     benchmarker.run_warmup()
     benchmarker.run_all_benches()
-
     benchmarker.comment_pr(pr_id)
-    os.chdir('..')
-    subprocess.call('rm -rf cloned_repo', shell=True)
+
+    clean_repo()
 
 
 if __name__ == '__main__':
