3ec58d5f1dae71fc62c70ffb958bd4e272066a93
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2017-05-02 14:56:50 +0200

    Refactor the scripts used in CI

---
+++ scripts/ci/install-travis.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -ex
+# Install tools for Travis CI build
+
+# Build and cache kcov
+if [ -f $HOME/local/bin/kcov ]; then
+    echo "Using cached kcov from ~/local/bin/kcov"
+else
+    mkdir -p $HOME/local
+    cd $HOME/local
+    wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz
+    tar xzf master.tar.gz && mkdir kcov-master/build && cd kcov-master/build
+    cmake -DCMAKE_INSTALL_PREFIX=$HOME/local ..
+    make install
+    cd $TRAVIS_BUILD_DIR
+fi
+
+# Build and cache mdbook
+if [ -f $HOME/.cargo/bin/mdbook ]; then
+    echo "Using cached mdbook from ~/.cargo/bin/mdbook"
+else
+    cargo install mdbook
+fi
+
+cd $TRAVIS_BUILD_DIR
+
+pip install --user -r scripts/ci/requirements.txt
---
+++ scripts/ci/setup-travis.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -ex
+# Setup environement variables for Travis CI build
+
+export PATH=$HOME/.local/bin:$PATH:$HOME/local/bin:$HOME/.cargo/bin
+export RUSTFLAGS="-C link-dead-code"
+
+if [[ "$TRAVIS_RUST_VERSION" == "stable" && "$TRAVIS_OS_NAME" == "linux" ]]; then
+    export DO_COVERAGE=true
+else
+    export DO_COVERAGE=false
+fi
+
+
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    export CC=gcc-4.9
+    export CXX=g++-4.9
+fi
