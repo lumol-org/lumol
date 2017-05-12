3275ec21680bdfd619004d4548d90e55c50487b7
antoinewdg <antoinewendlinger@gmail.com>
2017-05-12 15:12:39 +0200

    Try also parallelizing outer loop

--- src/core/src/energy/global/ewald.rs
+++ src/core/src/energy/global/ewald.rs
@@ -396,40 +396,36 @@ impl Ewald {
         let (rec_kx, rec_ky, rec_kz) = system.cell().reciprocal_vectors();
         let thread_forces_store = ThreadLocalStore::new(|| vec![Vector3D::zero(); system.size()]);
 
-        for ikx in 0..self.kmax {
-            for iky in 0..self.kmax {
-                for ikz in 0..self.kmax {
-                    // The k = 0 and the cutoff in k-space are already handled
-                    // in `expfactors`.
-                    let expfactor = self.expfactors[(ikx, iky, ikz)].abs();
-                    if expfactor < f64::EPSILON { continue }
+        Zip::indexed(&*self.expfactors).par_apply(|(ikx, iky, ikz), expfactor| {
+            // The k = 0 and the cutoff in k-space are already handled
+            // in `expfactors`.
+            let expfactor = expfactor.abs();
+            if expfactor < f64::EPSILON { return; }
 
-                    let f = expfactor * factor;
-                    let k = (ikx as f64) * rec_kx + (iky as f64) * rec_ky + (ikz as f64) * rec_kz;
+            let f = expfactor * factor;
+            let k = (ikx as f64) * rec_kx + (iky as f64) * rec_ky + (ikz as f64) * rec_kz;
 
-                    (0..system.size()).into_par_iter().for_each(|i| {
-                        let qi = system[i].charge;
+            (0..system.size()).into_par_iter().for_each(|i| {
+                let qi = system[i].charge;
 
-                        let fourier_i = self.fourier_phases[(ikx, i, 0)] *
-                            self.fourier_phases[(iky, i, 1)] *
-                            self.fourier_phases[(ikz, i, 2)];
-                        let fourier_i = fourier_i.imag();
+                let fourier_i = self.fourier_phases[(ikx, i, 0)] *
+                    self.fourier_phases[(iky, i, 1)] *
+                    self.fourier_phases[(ikz, i, 2)];
+                let fourier_i = fourier_i.imag();
 
-                        let mut thread_forces = thread_forces_store.borrow_mut();
-                        let mut force_i = Vector3D::zero();
+                let mut thread_forces = thread_forces_store.borrow_mut();
+                let mut force_i = Vector3D::zero();
 
-                        for j in (i + 1)..system.size() {
-                            let qj = system[j].charge;
-                            let force = f * self.kspace_force_factor(j, ikx, iky, ikz, qi, qj, fourier_i) * k;
-                            force_i -= force;
-                            thread_forces[j] += force;
-                        }
-
-                        thread_forces[i] += force_i;
-                    });
+                for j in (i + 1)..system.size() {
+                    let qj = system[j].charge;
+                    let force = f * self.kspace_force_factor(j, ikx, iky, ikz, qi, qj, fourier_i) * k;
+                    force_i -= force;
+                    thread_forces[j] += force;
                 }
-            }
-        }
+
+                thread_forces[i] += force_i;
+            });
+        });
 
         thread_forces_store.sum_local_values(forces);
 
