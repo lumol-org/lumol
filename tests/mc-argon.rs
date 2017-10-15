717e0699dc6e2b8df417814fe11685477a4e23ce
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2017-10-15 22:38:11 +0200

    Add a test for NPT monte carlo

---
+++ tests/mc-argon.rs
@@ -0,0 +1,35 @@
+// Lumol, an extensible molecular simulation engine
+// Copyright (C) Lumol's contributors — BSD license
+extern crate lumol;
+extern crate lumol_input as input;
+extern crate env_logger;
+
+use lumol::units;
+use input::Input;
+
+use std::path::Path;
+use std::sync::{Once, ONCE_INIT};
+static START: Once = ONCE_INIT;
+
+mod utils;
+
+#[test]
+fn npt() {
+    START.call_once(|| {env_logger::init().unwrap();});
+    let path = Path::new(file!()).parent().unwrap()
+                                 .join("data")
+                                 .join("mc-argon")
+                                 .join("npt.toml");
+
+    let mut config = Input::new(path).unwrap().read().unwrap();
+
+    let collecter = utils::Collecter::new(500);
+    let pressures = collecter.pressures();
+
+    config.simulation.add_output(Box::new(collecter));
+    config.simulation.run(&mut config.system, config.nsteps);
+
+    let expected = units::from(200.0, "bar").unwrap();
+    let pressure = ::utils::mean(pressures.clone());
+    assert!(f64::abs(pressure - expected) / expected < 5e-3);
+}
