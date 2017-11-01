81d45022b203be5cb0fc848cc147d9ae8b7b66fd
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2017-11-01 21:46:23 +0100

    Add started/ended and elapsed time to the output

--- src/bin/lumol.rs
+++ src/bin/lumol.rs
@@ -6,9 +6,12 @@ extern crate lumol_input;
 #[macro_use]
 extern crate log;
 extern crate clap;
+extern crate chrono;
 
 use lumol_input::Input;
 use clap::{App, ArgMatches};
+use chrono::offset::Local;
+use chrono::Duration;
 
 fn parse_args<'a>() -> ArgMatches<'a> {
     App::new("lumol")
@@ -31,5 +34,35 @@ fn main() {
 
     info!("Running lumol version {}", lumol::VERSION);
 
+    let start = Local::now();
+    info!("Simulation started the {} at {}", start.format("%Y-%m-%d"), start.format("%H:%M:%S"));
     config.simulation.run(&mut config.system, config.nsteps);
+
+    let end = Local::now();
+    info!("Simulation ended the {} at {}", end.format("%Y-%m-%d"), end.format("%H:%M:%S"));
+    let elapsed = end.signed_duration_since(start);
+    info!("Simulation ran for {}", format_elapsed(elapsed));
+}
+
+fn format_elapsed(elapsed: Duration) -> String {
+    if elapsed.num_weeks() > 0 {
+        let h = elapsed.num_hours() % 24;
+        let d = elapsed.num_days() % 7;
+        let w = elapsed.num_weeks();
+        format!("{} weeks {} days {}h", w, d, h)
+    } else if elapsed.num_days() > 0 {
+        let m = elapsed.num_minutes() % 60;
+        let h = elapsed.num_hours() % 24;
+        let d = elapsed.num_days();
+        format!("{} days {}h {}min", d, h, m)
+    } else if elapsed.num_hours() > 0 {
+        let s = elapsed.num_seconds() % 60;
+        let m = elapsed.num_minutes() % 60;
+        let h = elapsed.num_hours();
+        format!("{}h {}min {}s", h, m, s)
+    } else {
+        let s = elapsed.num_seconds() % 60;
+        let m = elapsed.num_minutes();
+        format!("{}min {}s", m, s)
+    }
 }
