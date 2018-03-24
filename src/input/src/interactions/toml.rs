f97ef809283a0760695f8e34c5a2ad71541fac01
Gernot Bauer <bauer@itt.uni-stuttgart.de>
2018-03-24 14:35:42 +0100

    Added entry for reference

--- src/input/src/interactions/toml.rs
+++ src/input/src/interactions/toml.rs
@@ -2,7 +2,7 @@
 // Copyright (C) Lumol's contributors — BSD license
 
 //! Convert TOML values to Lumol types.
-use toml::value::Table;
+
 
 use FromToml;
 use FromTomlWithData;
@@ -14,6 +14,7 @@ use lumol::energy::{CosineHarmonic, Harmonic, LennardJones, NullPotential, Mie};
 use lumol::energy::{Ewald, Wolf};
 use lumol::energy::{PairPotential, TableComputation};
 use lumol::units;
+use toml::value::Table;
 
 impl FromToml for NullPotential {
     fn from_toml(_: &Table) -> Result<NullPotential> {
@@ -45,20 +46,11 @@ impl FromToml for LennardJones {
 
 impl FromToml for Mie {
     fn from_toml(table: &Table) -> Result<Mie> {
-        let sigma = try_extract_parameter!(table, "sigma", "Mie potential");
-        let epsilon = try_extract_parameter!(table, "epsilon", "Mie potential");
-        let m = try_extract_parameter!(table, "m", "Mie potential");
-        let n = try_extract_parameter!(table, "n", "Mie potential");
-
-        if let (Some(sigma), Some(epsilon), Some(m), Some(n)) =
-            (sigma.as_str(), epsilon.as_str(), m.as_float(), n.as_float()) {
-            let sigma = try!(::lumol::units::from_str(sigma));
-            let epsilon = try!(::lumol::units::from_str(epsilon));
-            Ok(Mie::new(sigma, epsilon, n, m))
-        } else {
-            Err(Error::from("'epsilon' and 'sigma' must be strings \
-                            and 'm' and 'n' must be floats in Mie potential"))
-        }
+        let sigma = extract::str("sigma", table, "Mie potential")?;
+        let epsilon = extract::str("epsilon", table, "Mie potential")?;
+        let m = extract::number("m", table, "Mie potential")?;
+        let n = extract::number("n", table, "Mie potential")?;
+        Ok(Mie::new(units::from_str(sigma)?, units::from_str(epsilon)?, n as f64, m as f64))
     }
 }
 
@@ -145,9 +137,9 @@ impl FromTomlWithData for TableComputation {
     type Data = Box<PairPotential>;
 
     fn from_toml(table: &Table, potential: Box<PairPotential>) -> Result<TableComputation> {
-        let table = try!(table["table"].as_table().ok_or(
-            Error::from("'table' key in computation must be a TOML table")
-        ));
+        let table = try!(table["table"]
+            .as_table()
+            .ok_or(Error::from("'table' key in computation must be a TOML table")));
         let n = extract::uint("n", table, "table computation")?;
         let max = extract::str("max", table, "table computation")?;
         Ok(TableComputation::new(potential, n as usize, units::from_str(max)?))
