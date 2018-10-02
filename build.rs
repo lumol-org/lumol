fe8fd385f4607a83ff43d0eb56ddd32eb1215e8d
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2018-10-02 15:54:50 +0000

    Specify bench/test/doctest=false where needed

---
+++ build.rs
@@ -0,0 +1,14 @@
+use std::process::Command;
+use std::str;
+
+fn main() {
+    let mut version = env!("CARGO_PKG_VERSION").to_string();
+    if let Ok(output) = Command::new("git").args(&["describe", "--always"]).output() {
+        let git = str::from_utf8(&output.stdout).unwrap().trim();
+        version.push_str("-");
+        version.push_str(git);
+    }
+
+    println!("cargo:rustc-env=LUMOL_FULL_GIT_VERSION={}", version);
+    println!("cargo:rerun-if-changed=.git/HEAD");
+}
