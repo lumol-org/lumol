cc49ff3b7c52940686c3b8b102dba66a2020e71e
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2020-11-04 10:07:25 +0100

    Fix Travis CI on osx

--- scripts/ci/install-travis.sh
+++ scripts/ci/install-travis.sh
@@ -5,6 +5,10 @@
 if [ -f $HOME/local/bin/kcov ]; then
     echo "Using cached kcov from ~/local/bin/kcov"
 else
+    if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
+        export OPENSSL_ROOT_DIR=$(brew --prefix openssl)
+    fi
+
     mkdir -p $HOME/local
     cd $HOME/local
     wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz
